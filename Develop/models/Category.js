2110a696992a782a961fbde443e71adf13c1a849
Ancandy <liuya@uci.edu>
2022-10-03 12:11:20 -0700

    major work done

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define the id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define th category name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,42 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+  }, 
+  // define the product_name column
+  product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+  // define the price column
+  price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+          isDecimal: true
+      }
+  },
+  // define the stock column
+  stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+          isNumeric: true
+      }
+  },
+  // define the category_id column
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+        model: "category",
+        key: "id"
+    }
+  }
   },
   {
     sequelize,
